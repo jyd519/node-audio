3390bb845c31982537999de6530d2184cceafbbd
jyd519 <jiyongdong@ata.net.cn>
2021-02-07 13:51:09 +0800

    add: tests

---
+++ test/fixup_webm.test.js
@@ -0,0 +1,21 @@
+const addon = require('../x64/audio.node');
+
+it('fixup_webm sync', () => {
+  let r = addon.fixup_webm(`${__dirname}/test.webm`, './a.webm');
+  expect(r).toEqual(0);
+});
+
+it('fixup_webm sync failed', () => {
+  let r = addon.fixup_webm(`${__dirname}/notexists.webm`, './a.webm');
+  expect(r).toEqual(1);
+});
+
+it('fixup_webm_async', async () => {
+  let r = await addon.fixup_webm_async(`${__dirname}/test.webm`, './a.webm');
+  expect(r).toEqual(0);
+});
+
+it('fixup_webm_async failed', async () => {
+  let r = await addon.fixup_webm_async(`${__dirname}/notexists.webm`, './a.webm');
+  expect(r).toEqual(1);
+});
